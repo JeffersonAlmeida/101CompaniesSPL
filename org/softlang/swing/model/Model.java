e7b526eaf08731da33a0f245d5178e5fae554802
unknown <Jefferson@Jefferson-PC.(none)>
2012-08-28 17:52:16 -0300

    Conditional Compilation in model.java has been added in order to permit calculate salaries with several features.

--- org/softlang/swing/model/Model.java
+++ org/softlang/swing/model/Model.java
@@ -1,10 +1,18 @@
 package org.softlang.swing.model;
 
+import static org.junit.Assert.assertEquals;
+
+import org.softlang.command.CutCompany;
+import org.softlang.command.CutEmployee;
 import org.softlang.company.Company;
 import org.softlang.company.Department;
 import org.softlang.company.Employee;
 import org.softlang.company.factory.Factory;
 import org.softlang.company.factory.PojoFactory;
+import org.softlang.features.OrderedCut;
+import org.softlang.features.SimpleCut;
+import org.softlang.features.TotalReducer;
+import org.softlang.features.TotalWalker;
 import org.softlang.operations.Cut;
 import org.softlang.operations.Total;
 
@@ -25,10 +33,6 @@ public class Model {
 		
 		// The serialized object has been copied over from elsewhere	
 		company = (org.softlang.company.impl.pojo.CompanyImpl)createSampleCompany(new PojoFactory());
-		
-		System.out.println("qto dpts: " + company.getDepts().size());
-		//company.writeObject("sampleCompany.ser");
-		//((CompanyImpl) company).readObject("sampleCompany.ser");
 		//company = new CompanyImpl().readObject("sampleCompany.ser");
 	}
 
@@ -37,27 +41,16 @@ public class Model {
 	 * or employee.
 	 * 
 	 * @return current total value
-	 */
-	/*public String getTotal() {
-		if (currentValue != null) {
-			if (currentValue.isCompany()) {
-				return Double.toString(Total.total((Company) currentValue));
-			} else if (currentValue.isDepartment()) {
-				return Double.toString(Total.total((Department) currentValue));
-			} else if (currentValue.isEmployee()) {
-				return Double.toString(Total.total((Employee) currentValue));
-			} else {
-				return "0";
-			}
-		} else {
-			return "0";
-		}
-	}*/
-	
+	 *//*
 	public String getTotal() {
 		if (currentValue != null) {
 			if (currentValue instanceof Company) {
 				return Double.toString(Total.total((Company) currentValue));
+				#if($TotalWalker)
+				    TotalWalker walker = new TotalWalker();
+				    walker.preorder(currentValue);
+				    return Double.toString(walker.getTotal());
+				/*#end
 			} else if (currentValue instanceof Department) {
 				return Double.toString(Total.total((Department) currentValue));
 			} else if (currentValue instanceof Employee) {
@@ -68,23 +61,41 @@ public class Model {
 		} else {
 			return "0";
 		}
-	}
-
-	/*public String getTotal() {
-		TotalReducer total = new TotalReducer();
-		if (currentValue != null) {
-			return Double.toString(total.reduce (currentValue));
-		}
-		else {
-			return "0";
-		}
 	}*/
 	
+	/*#if($TotalWalker)*/
+	/**
+	 * This method returns the total value for the current company, department
+	 * or employee.
+	 * 
+	 * @return current total value
+	 *//*
+	public String getTotal() {
+		TotalWalker walker = new TotalWalker();
+	    walker.postorder(currentValue);
+	    return Double.toString(walker.getTotal());
+	}*/
+	/*#end*/
 	
+	/*#if($TotalReducer)*/
+	/**
+	 * This method returns the total value for the current company, department
+	 * or employee.
+	 * 
+	 * @return current total value
+	 */
+	public String getTotal() {
+		TotalReducer total = new TotalReducer();
+		double value = total.reduce(currentValue);
+		return Double.toString(value);
+	}
+	//#end*/
+
+		
 	/**
 	 * This method cuts the current company, department or employee.
 	 */
-	public void cut() {
+	/*public void cut() {
 		if (currentValue != null) {
 			if (currentValue instanceof Company) {
 				Cut.cut((Company) currentValue);
@@ -94,7 +105,32 @@ public class Model {
 				Cut.cut((Employee) currentValue);
 			}
 		}
+	}*/
+	
+	/*#if($SimpleCut)*/
+	/*public void cut() {
+		SimpleCut cut = new SimpleCut();
+		cut.postorder(currentValue);
+	}*/
+	//#end*/
+	
+	/*#if($CommandCut)*/
+	/*public void cut() {
+		if(currentValue instanceof Company){
+			CutCompany cut = new CutCompany((Company) currentValue);
+			cut.execute();
+		}else if (currentValue instanceof Employee){
+			new CutEmployee((Employee) currentValue).execute();
+		}		 
+	}*/
+	//#end*/
+	
+	/*#if($requestCut)*/
+	public void cut(){
+		OrderedCut cut = new OrderedCut();
+		cut.postorder(currentValue);
 	}
+	//#end*/
 
 	// ----------------------------------- GETTER & SETTER
 	public void setName(String newValue) {
